c4bcb3cf5b7f18d4300f52ec8534f298ae284691
buranov417 <husanburanov417@gmail.com>
2025-10-26 12:20:05 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function openSection(section) {
+  const content = document.getElementById('content');
+  content.innerHTML = `<p>Раздел <b>${section}</b> пока в разработке. Здесь будут фото, файлы и аудио из Telegram.</p>`;
+}
