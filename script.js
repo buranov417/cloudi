aab20a86782444d103438ef2d616eec5d04391ec
buranov417 <husanburanov417@gmail.com>
2025-11-15 14:28:05 +0500

    Н

--- script.js
+++
@@ -1,4 +0,0 @@
-function openSection(section) {
-  const content = document.getElementById('content');
-  content.innerHTML = `<p>Раздел <b>${section}</b> пока в разработке. Здесь будут фото, файлы и аудио из Telegram.</p>`;
-}
